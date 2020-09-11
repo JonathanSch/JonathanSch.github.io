efbb14fdea0767a39019487087c00824673fe219
JonathanSch <jonathansch7@gmail.com>
2020-09-11 17:46:02 -0500

    cambios pertinentes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const boton = document.getElementById('boton')
+let nombre = document.getElementById('nombre');
+let titulo = document.getElementById('titulo')
+const boton = document.getElementById('lopaterick')
 
 boton.addEventListener('click', () => {
-    alert('Felicidades, has innovado')
+    if (nombre.value == '') {
+        titulo.innerHTML = 'No tienes nombre ponlo antes bb'
+    }
+    else {
+        titulo.innerHTML = `Hola ${nombre.value}, mi nombre es Erick Lopata y te saludo amigo`
+    }
+
 })
